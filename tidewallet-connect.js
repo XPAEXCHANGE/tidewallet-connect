6b2ca16e2d1a51e00733a5cf9f9b9063d86f728c
Nino Lin <nino.lin@isuncloud.com>
2018-06-29 16:20:17 +0800

    complete v1 functioon

--- tidewallet-connect.js
+++ tidewallet-connect.js
@@ -64,55 +64,47 @@ class TWC {
   	}
   }
 
-  //--
-  static randomWait(result) {
-    return new Promise((resolve, reject) => {
-    	setTimeout(() => {
-    	  this.randomSuccess() ?
-    	    resolve(result) :
-    	    reject(new Error('oops'));
-    	}, Math.random() * 3000 + 1000);
-    });
-  }
-  static randomSuccess() {
-  	return Math.random() > 0.1;
-  }
-  static log(fn, args) {
-  	console.log(`--- ${fn} ---`);
-  	console.log(args);
-  }
-  //--
-
-  static TidewalletCommand({ cmd, address, tx, from, to, value, data }) {
+  static TidewalletCommand({ cmd, address, tx, from, to, token, value, data }) {
   	const tmpA = document.createElement('a');
   	const rid = this.randomID();
-  	console.log(rid);
   	tmpA.target = '_blank'
     switch(cmd) {
       case 'accounts':
-        const params = "";
-        tmpA.href = `tidewallet://connect/accounts/${rid}?${params}`;
-      break;
-
+        tmpA.href = `tidewallet://connect/accounts/${rid}`;
+        break;
       case 'getBalance':
-      break;
-
+        tmpA.href = `tidewallet://connect/getBalance/${rid}?address=${address}`;
+        break;
       case 'getTokenBalance':
-      break;
-
+        tmpA.href = `tidewallet://connect/getTokenBalance/${rid}?address=${address}&token=${token}`;
+        break;
       case 'getTransactionReceipt':
-      break;
-
+        tmpA.href = `tidewallet://connect/getTransactionReceipt/${rid}?tx=${tx}`;
+        break;
       case 'call':
-      break;
-
+        tmpA.href = `tidewallet://connect/call/${rid}?from=${from}?to=${to}?value=${value}?data=${data}`;
+        break;
       case 'sendTransaction':
-      break;
+        tmpA.href = `tidewallet://connect/sendTransaction/${rid}?from=${from}?to=${to}?value=${value}?data=${data}`;
+        break;
     }
 
     return new Promise((resolve, reject) => {
       this.once({ id: rid, callback: (data) => {
-      	resolve(data.result);
+        if(data){
+          if(cmd == 'call'){
+            let l = Math.floor(data.slice(2).length / 64);
+            let r = [];
+            for (let i = 0; i < l; i++) {
+                r.push(data.slice(2+i*64, 66+i*64));
+            }
+            resolve(r);
+          } else {
+            resolve(data);
+          }
+        } else {
+          reject(new Error('oops!'));
+        }
       }});
       tmpA.click();
     });
@@ -121,100 +113,48 @@ class TWC {
   	return this.TidewalletCommand({
   	  cmd: 'accounts'
   	});
-  	this.log('accounts');
-    return this.randomWait(['0x49aa79070f27f2fa4c375586eaf03c862cad26e9']);
   }
   static getBalance({ address }) {
-    this.log('getBalance', arguments[0]);
-    return this.randomWait(Math.random() * 10**18);
+    return this.TidewalletCommand({
+      cmd: 'getBalance',
+      address: address
+  	});
   }
-  static getTokenBalance({ address }) {
-    this.log('getBalance', arguments[0]);
-    return this.randomWait(Math.random() * 10**18);
+  static getTokenBalance({ address, token }) {
+    return this.TidewalletCommand({
+      cmd: 'getTokenBalance',
+      address: address,
+      token: token
+  	});
   }
   static getTransactionReceipt({ tx }) {
-    this.log('getTransactionReceipt', arguments[0]);
-    const result = Math.random() > 0.5 ?
-      {
-        "blockHash": "0xd96a04602a1c19d19f8c854f8339290d513ef2cff058d941859e6cc36177c03d",
-        "blockNumber": "0x58fbe9",
-        "contractAddress": null,
-        "cumulativeGasUsed": "0x734905",
-        "gasUsed": "0xcb51",
-        "logs": [
-          {
-            "address": "0xfb8bf095ebcdad57d2e37573a505e7d3bafdd3cc",
-            "blockHash": "0xd96a04602a1c19d19f8c854f8339290d513ef2cff058d941859e6cc36177c03d",
-            "blockNumber": "0x58fbe9",
-            "data": "0x00000000000000000000000000000000000000000000000000000000b2d05e00",
-            "logIndex": "0x95",
-            "topics": [
-              "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-              "0x000000000000000000000000b1f545c55322d4bee27eda5d8cf33ad63a9db81c",
-              "0x0000000000000000000000008ac8a3353020b4167253663ee43b8e6fd3755e1d"
-            ],
-            "transactionHash": "0x764191f33228fb917eff0c6cc6d34838c8bf6004d38e3f2ede7b172d5fe3ceff",
-            "transactionIndex": "0x6f",
-            "transactionLogIndex": "0x0",
-            "type": "mined"
-          }
-        ],
-        "logsBloom": "0x00000000000001000000000000000000000000000040000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000001000008000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000080000200000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000400000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000",
-        "root": null,
-        "status": "0x1",
-        "transactionHash": tx,
-        "transactionIndex": "0x6f"
-      } :
-      null;
-    return this.randomWait(result);
+    return this.TidewalletCommand({
+      cmd: 'getTransactionReceipt',
+      tx: tx
+  	});
   }
   static call({ from, to, value, data }) {
-    this.log('getTransactionReceipt', arguments[0]);
-    let result;
-    if(/da359a24/.test(data)) {  // gameSets(address,uint256,uint256)
-      result = '0x' + ((parseInt(Math.random() * 10) + 1) * 10 ** 18).toString(16);
-    } else if(/a61d0aed/.test(data)) {  // maxBets()
-      result = '0x' + (10 * 10 ** 18).toString(16);
-    } else if(/118ea1af/.test(data)) {  // getTotalStakes(address,uint256)
-      result = '0x' + ((parseInt(Math.random() * 10) + 1) * 10 ** 18).toString(16);
-    } else if(/d4b19a1d/.test(data)) {  // period(address)
-      result = '0x' + (parseInt(Math.random() * 100)).toString(16);
-    } else if(/8143c7e6/.test(data)) {  // unclaimedReward(address,uint256,address)
-      result = Math.random() > 0.7 ?
-        '0x0000000000000000000000000000000000000000000000000000000000000001' :
-        '0x0000000000000000000000000000000000000000000000000000000000000000';
-    } else if(/aeeaf377/.test(data)) {  // tickets(address,uint256,address)
-      result = '0x' + (parseInt(Math.random() * 11) + 2).toString(16);
-    } else if(/7724f4e7/.test(data)) {  // results(address,uint256)
-      result = '0x' + (parseInt(Math.random() * 11) + 2).toString(16);
-    } else if(/565b1a25/.test(data)) {  // getCurrentRound(address)
-      result = [
-        '0x' + (parseInt(Math.random() * 100)).toString(16),
-        '0x' + ((parseInt(Math.random() * 10) + 1) * 10 ** 18).toString(16),
-        '0x' + (10 * 10 ** 18).toString(16)
-      ];
-    } else if(/e62c06af/.test(data)) {
-      const r1 = Math.random() > 0.8;
-      const b1 = r1 ?
-        '0x0000000000000000000000000000000000000000000000000000000000000001' :
-        '0x0000000000000000000000000000000000000000000000000000000000000000';
-      const b2 = r1 && Math.random() > 0.5 ?
-        '0x0000000000000000000000000000000000000000000000000000000000000001' :
-        '0x0000000000000000000000000000000000000000000000000000000000000000'
-      result = [b1, b2];
-    } else {
-      result = '0x' + (parseInt(Math.random() * 10 ** 18)).toString(16);
-    }
-    return this.randomWait(result);
+    return this.TidewalletCommand({
+      cmd: 'call',
+      from: from,
+      to: to,
+      value: value,
+      data: data
+  	});
   }
   static sendTransaction({ from, to, value, data }) {
-  	this.log('sendTransaction', arguments[0]);
-  	return this.randomWait('0xe670ec64341771606e55d6b4ca35a1a6b75ee3d5145a99d05921026d1527331');
+    return this.TidewalletCommand({
+      cmd: 'sendTransaction',
+      from: from,
+      to: to,
+      value: value,
+      data: data
+  	});
   }
 }
-const TidewalletEmit = ({ id, data }) => {
+const TidewalletEmit = ({ id, result }) => {
   if(events[id] instanceof Function) {
-  	events[id](data);
+  	events[id](result);
   	delete events[id];
   }
 };
